15ba08018736eb9d01f97bf3289584f2902e43fa
yerias <1748094638@qq.com>
2022-03-20 20:58:43 +0800

    111

--- tunan-tag-model/src/main/java/com/tunan/hello.java
+++
@@ -1,9 +0,0 @@
-package com.tunan;
-
-/**
- * @Auther: 李沅芮
- * @Date: 2022/3/20 17:15
- * @Description:
- */
-public class hello {
-}
---
+++ tunan-tag-model/src/main/scala/com/tunan/tag/ModelType.java
@@ -0,0 +1,13 @@
+package com.tunan.tag;
+
+/**
+ * 模型类型：规则匹配、统计和挖掘
+ */
+public enum ModelType {
+    // 规则匹配
+    MATCH,
+    // 挖掘
+    ML,
+    // 统计
+    STATISTICS
+}
