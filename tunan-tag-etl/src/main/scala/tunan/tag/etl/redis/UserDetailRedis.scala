c27f94622985be622f133cbfa141d7034aa772e4
yerias <1748094638@qq.com>
2022-04-24 23:03:15 +0800

    20220324

--- tunan-tag-etl/src/main/scala/tunan/tag/etl/redis/UserDetailRedis.scala
+++ tunan-tag-etl/src/main/scala/tunan/tag/etl/redis/UserDetailRedis.scala
@@ -24,16 +24,16 @@ object UserDetailRedis {
 		// spark 计算
 
 		val totalDetails = new util.ArrayList[TotalDetail]
-		totalDetails.add(new TotalDetail(101, -0.22, "22%", Array[Integer](6, 7, 1, 3, 9, 10, 5), Array[String]("04/01", "04/02", "04/03", "04/04", "04/05", "04/06", "04/07")))
-		totalDetails.add(new TotalDetail(102, 0.16, "16%", Array[Integer](3, 3, 3, 9, 6, 9, 6), Array[String]("04/01", "04/02", "04/03", "04/04", "04/05", "04/06", "04/07")))
-		totalDetails.add(new TotalDetail(103, -0.22, "22%", Array[Integer](1, 3, 1, 8, 7, 4, 5), Array[String]("04/01", "04/02", "04/03", "04/04", "04/05", "04/06", "04/07")))
-		totalDetails.add(new TotalDetail(104, 0.39, "39%", Array[Integer](6, 7, 6, 3, 55, 10, 7), Array[String]("04/01", "04/02", "04/03", "04/04", "04/05", "04/06", "04/07")))
+		totalDetails.add(new TotalDetail(301, -0.06, "-6%", Array[Integer](6, 7, 1, 3, 9, 10, 5), Array[String]("04/01", "04/02", "04/03", "04/04", "04/05", "04/06", "04/07")))
+		totalDetails.add(new TotalDetail(302, 0.19, "+19%", Array[Integer](3, 3, 3, 9, 6, 9, 6), Array[String]("04/01", "04/02", "04/03", "04/04", "04/05", "04/06", "04/07")))
+		totalDetails.add(new TotalDetail(303, 0.43, "+43%", Array[Integer](1, 3, 1, 8, 7, 4, 5), Array[String]("04/01", "04/02", "04/03", "04/04", "04/05", "04/06", "04/07")))
+		totalDetails.add(new TotalDetail(304, -0.18, "-39%", Array[Integer](6, 7, 6, 3, 55, 10, 7), Array[String]("04/01", "04/02", "04/03", "04/04", "04/05", "04/06", "04/07")))
 		val totalDetailsJson = JSON.toJSON(totalDetails).toString
 
 
 		val userDetails = new util.HashMap[String, Object]()
-		userDetails.put("line",new UserDetail(Array[Integer](55, 99, 33, 66, 99, 10, 55), Array[String]("04/01", "04/02", "04/03", "04/04", "04/05", "04/06", "04/07")))
-		userDetails.put("bar",new UserDetail(Array[Integer](11, 22, 33, 44, 55, 66), Array[String]("章节练习目录页", "app-课程目录页", "app首页", "我的", "题库首页", "签到首页")))
+		userDetails.put("line",new UserDetail(Array[Integer](200, 99, 33, 66, 99, 10, 55), Array[String]("04/01", "04/02", "04/03", "04/04", "04/05", "04/06", "04/07")))
+		userDetails.put("bar",new UserDetail(Array[Integer](201, 22, 33, 44, 55, 66), Array[String]("章节练习目录页", "app-课程目录页", "app首页", "我的", "题库首页", "签到首页")))
 
 		val userDetailsJson = JSON.toJSON(userDetails).toString
 
@@ -42,8 +42,8 @@ object UserDetailRedis {
 		try {
 			jedis = RedisUtils.getJedis
 
-			jedis.hset("total_detail", "total_detail", totalDetailsJson)
-			jedis.hset("user_detail", "112233", userDetailsJson)
+			jedis.hset("total_detail_7", "total_detail", totalDetailsJson)
+			jedis.hset("user_detail_7", "112233", userDetailsJson)
 		} catch {
 			case e: Exception =>
 				e.printStackTrace()
